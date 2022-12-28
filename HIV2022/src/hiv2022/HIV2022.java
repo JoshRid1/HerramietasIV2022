4773b1dfa2a1962fae5cefe5adc4f438b6642eed
JOSEPH <JOSEPH@DESKTOP-J0AABQD>
2022-12-28 08:25:20 -0500

    Segunda Subida de proyecto de H IV- 2022 II

--- HIV2022/src/hiv2022/HIV2022.java
+++ HIV2022/src/hiv2022/HIV2022.java
@@ -14,7 +14,10 @@ public class HIV2022 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x = 4;
+        int y = 5;
+        int suma = x + y;
+        System.out.println("La suma es:"+suma);
     }
     
 }
