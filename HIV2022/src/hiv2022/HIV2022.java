5bfeeb52c371df06a6532e95919749d4820b9dcf
JoshRid1 <121555997+JoshRid1@users.noreply.github.com>
2022-12-29 07:33:54 -0500

    Tercera actualización para Herramientas IV - II

--- HIV2022/src/hiv2022/HIV2022.java
+++ HIV2022/src/hiv2022/HIV2022.java
@@ -16,7 +16,8 @@ public class HIV2022 {
     public static void main(String[] args) {
         int x = 4;
         int y = 5;
-        int suma = x + y;
+        int z =7;
+        int suma = x + y + z;
         System.out.println("La suma es:"+suma);
     }
     
